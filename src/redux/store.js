1e9b833d91283d7a119b53dfbb79c2c91ae24eaf
Irfnekf <vladshkaraputa07@gmail.com>
2023-03-08 10:58:31 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { persistStore } from 'redux-persist';
 
 import rootReducer from './root-reducer';
 
 export const store = configureStore({
   reducer: rootReducer,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: false,
-    }),
 });
-
-export const persistor = persistStore(store);
